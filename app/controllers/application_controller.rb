9427bb0c49bfca790271a9b0681227b0cef99d24
tiwakawa <tiwakawa@aiming-inc.com>
2012-07-27 20:26:50 +0900

    refactoring current_user function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @current_user ||= if session[:user_id]
-                        User.find(session[:user_id])
-                      end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
