02bf357dfb83ed6d8989879f8e9b6e6f89cf934a
tiwakawa <tiwakawa@aiming-inc.com>
2012-07-31 12:03:14 +0900

    add index to topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730025420) do
+ActiveRecord::Schema.define(:version => 20120731030036) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20120730025420) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "topics", ["user_id"], :name => "index_topics_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "provider"
